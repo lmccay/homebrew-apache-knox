c63b31025699fe6b225ca5dd989a2cbd5f9e7dc0
lmccay <lmccay@hortonworks.com>
2020-04-28 10:50:18 -0400

    Update apache-knox-shell.rb

--- Formula/apache-knox-shell.rb
+++ Formula/apache-knox-shell.rb
@@ -28,9 +28,9 @@ class ApacheKnoxShell < Formula
 
       # Some binaries have really generic names (like `test`) and most seem to be
       # too special-purpose to be permanently available via PATH.
-      #%w[gateway.sh ldap.sh].each do |script|
-      #  (bin/script).write_env_script "#{libexec}/bin/#{script}", Language::Java.java_home_env("1.8")
-      #end
+      %w[gateway.sh ldap.sh].each do |script|
+        (bin/script).write_env_script "#{libexec}/bin/#{script}", Language::Java.java_home_env("1.8")
+      end
     end
   end
 
